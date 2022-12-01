5250dfc1da8135fee2ea7def47073496a66a1d02
Nick Sheward <nickshwrd@gmail.com>
2022-12-01 13:06:52 +0000

    Psuedo code created

---
+++ script.js
@@ -0,0 +1,14 @@
+// When the player inputs a choice = rock, paper or scissors (not case sensitive)
+// Computer makes a random choice from rock, paper or scissors (not case sensitive)
+
+// If the player chooses rock and the computer scissors return "You win, rock beats scissors" and the player receives a point
+    // Else if the player chooses rock and the computer paper return "You lose, paper beats rock" and the computer receives a point 
+    // Else if the player chooses scissors and the computer paper return "You win, scissors beat paper" and the player receives a point 
+    // Else if the player chooses scissors and the computer rock return "You lose, rock beats scissors" and the computer receives a point 
+    // Else if the player chooses paper and the computer rock return "You win, paper beats rock!" and the player receives a point 
+    // Else if the player chooses paper and the computer scissors return "You lose, scissors beat paper" and the computer receives a point 
+        // Else the player and computer make the same choice return "It's a drawer", and do not log a point to either the player or Computer
+        // note - also check the player has entered the correct input, i.e. rock, paper or scissors (not case sensitive!)
+
+// Run the game 5 times, totalling up the computer and player results each round 
+// Calculate who won at the end of the 5 rounds and return an appropriate message, i.e. the player has won 
