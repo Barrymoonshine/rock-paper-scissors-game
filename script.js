4c62025f3bfc4dec5f0b6e7d59a3db9e0a390efd
Nick Sheward <nickshwrd@gmail.com>
2022-12-22 11:31:18 +0000

    Tidied up CSS

--- script.js
+++ script.js
@@ -1,99 +1,32 @@
 const computerOptions = [`rock`, `paper`, `scissors`];
-
-function getComputerChoice() {
-  return (computerOptions[Math.floor(Math.random()*computerOptions.length)]);
-} 
+const container = document.getElementById(`container`);
+const playButtons = document.getElementsByClassName('playBtns');
+const btnReset = document.getElementById(`resetBtn`);
+const playListeners = function (e) {
+  playRound(e.target.id, getComputerChoice());
+  showResetBtn ();
+  trackScores();
+}
 
 let playerScore = 0;
 let computerScore = 0;
 
-let allBtns = document.querySelectorAll('button');
-
-for (i of allBtns) {
-  i.addEventListener('click', (e) => {
-    playRound(e.target.id, getComputerChoice());
-  });
-}
-
-// const btnRock = document.getElementsByClassName(`rock`);
-
-// function playerSelectionRock() {
-//   playRound(`rock`,getComputerChoice());
-//   resetBtnVisibility ();
-// }
-
-// btnRock[0].addEventListener(`click`, playerSelectionRock);
-
-// const btnPaper = document.getElementsByClassName(`paper`);
-
-// function playerSelectionPaper() {
-//   playRound(`paper`,getComputerChoice());
-//   resetBtnVisibility ();
-// }
-
-// btnPaper[0].addEventListener(`click`, playerSelectionPaper);
-
-// const btnScissors = document.getElementsByClassName(`scissors`);
-
-// function playerSelectionScissors() {
-//   playRound(`scissors`,getComputerChoice());
-//   resetBtnVisibility ();
-// }
-
-// btnScissors[0].addEventListener(`click`, playerSelectionScissors);
-
-const container = document.querySelector(`#container`);
-
-function playRound(playerSelection, computerSelection) {
- if (playerScore < 4 && computerScore < 4) {
-    if ((playerSelection === `rock` && computerSelection === `scissors`) || (playerSelection === `paper` && computerSelection === `rock`) || (playerSelection === `scissors` && computerSelection === `paper`)) {
-        return [playerScore +=1, container.innerHTML += `You win ${playerSelection} beats ${computerSelection}! Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`];
-    } else if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissors`) || (playerSelection === `scissors` && computerSelection === `rock`)) {
-        return [computerScore +=1, container.innerHTML += `You lose ${computerSelection} beats ${playerSelection}! Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`];
-    } else if (playerSelection === computerSelection ) {
-        return container.innerHTML += `It's a draw.  Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`;
-    }
- }
-  else if (playerScore == 4 && computerScore < 4) {
-    if ((playerSelection === `rock` && computerSelection === `scissors`) || (playerSelection === `paper` && computerSelection === `rock`) || (playerSelection === `scissors` && computerSelection === `paper`)) {
-        return [playerScore +=1, container.innerHTML += `You win ${playerSelection} beats ${computerSelection}! Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br /> <h2> You beat the super-computer, congrats! <h2>`];
-  } else if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissors`) || (playerSelection === `scissors` && computerSelection === `rock`)) {
-        return [computerScore +=1, container.innerHTML += `You lose ${computerSelection} beats ${playerSelection}! Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`];
-  } else if (playerSelection === computerSelection ) {
-        return container.innerHTML += `It's a draw.  Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`;
-  }
-}
-  else if (playerScore < 4 && computerScore == 4) { 
-    if ((playerSelection === `rock` && computerSelection === `scissors`) || (playerSelection === `paper` && computerSelection === `rock`) || (playerSelection === `scissors` && computerSelection === `paper`)) {
-        return [playerScore +=1, container.innerHTML += `You win ${playerSelection} beats ${computerSelection}! Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`];
-  } else if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissors`) || (playerSelection === `scissors` && computerSelection === `rock`)) {
-        return [computerScore +=1, container.innerHTML += `You lose ${computerSelection} beats ${playerSelection}! Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br /> <h2> The super-computer has won, run for your life! <h2>`];
-  } else if (playerSelection === computerSelection ) {
-        return container.innerHTML += `It's a draw.  Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`;
-  }
-}
-  else if (playerScore == 4 && computerScore == 4) { 
-    if ((playerSelection === `rock` && computerSelection === `scissors`) || (playerSelection === `paper` && computerSelection === `rock`) || (playerSelection === `scissors` && computerSelection === `paper`)) {
-        return [playerScore +=1, container.innerHTML += `You win ${playerSelection} beats ${computerSelection}! Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br /> <h2> The humans have won to fight another day! <h2>`];
-  } else if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissors`) || (playerSelection === `scissors` && computerSelection === `rock`)) {
-        return [computerScore +=1, container.innerHTML += `You lose ${computerSelection} beats ${playerSelection}! Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />  <h2> The super-computer has won, run for your life! <h2>`];
-  } else if (playerSelection === computerSelection ) {
-        return container.innerHTML += `It's a draw.  Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`;
-  }  
-}
-}
-
-const btnReset = document.getElementById(`resetBtn`);
-
-function resetGame() {
-  window.location.reload();
-}
+btnReset.style.visibility = `hidden`;
 
 btnReset.addEventListener(`click`, resetGame);
 
-resetBtn.style.visibility = `hidden`;
+function resetGame() {
+  container.innerHTML = "";
+  playerScore = 0;
+  computerScore = 0;
+  btnReset.style.visibility = `hidden`;
+  for (i of playButtons) {
+    i.style.cursor = `pointer`
+    i.addEventListener('click',  playListeners);
+  }
+}
 
-function resetBtnVisibility () {
+function showResetBtn () {
   if (playerScore < 5 && computerScore < 5) {
     resetBtn.style.visibility = `hidden`;
   }
@@ -101,3 +34,38 @@ function resetBtnVisibility () {
     resetBtn.style.visibility = `visible`;
   }
 }
+
+function getComputerChoice() {
+  return (computerOptions[Math.floor(Math.random()*computerOptions.length)]);
+}
+
+for (i of playButtons) {
+  i.addEventListener('click',  playListeners);
+}
+
+function playRound(playerSelection, computerSelection) {
+  if ((playerSelection === `rock` && computerSelection === `scissors`) || (playerSelection === `paper` && computerSelection === `rock`) || (playerSelection === `scissors` && computerSelection === `paper`)) {
+    [playerScore +=1, container.innerHTML = `You win ${playerSelection} beats ${computerSelection}! Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`];
+  } else if ((playerSelection === `rock` && computerSelection === `paper`) || (playerSelection === `paper` && computerSelection === `scissors`) || (playerSelection === `scissors` && computerSelection === `rock`)) {
+    [computerScore +=1, container.innerHTML = `You lose ${computerSelection} beats ${playerSelection}! Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`];
+  } else if (playerSelection === computerSelection ) {
+    container.innerHTML = [`It's a draw.  Your score is: ${playerScore}, the computers' score is: ${computerScore}. <br /> <br />`];
+  }
+ }
+
+ function trackScores () {
+  if (playerScore == 5) {
+    container.innerHTML += `<br /> <h2> You beat the super-computer, congrats! <h2>`;
+    removePlayEvtLsts ();
+  }  else if (computerScore == 5) {
+    container.innerHTML += `<br />  <h2> The super-computer has won, run for your life! <h2>`;
+    removePlayEvtLsts ();
+  }
+ }
+
+ function removePlayEvtLsts () {
+  for (i of playButtons) {
+    i.removeEventListener('click',  playListeners);
+    i.style.cursor = `not-allowed`;
+  }
+ }
